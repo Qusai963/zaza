0beceb25ec18905f07b3b806ccb012563cc3c5ff
Qusai963 <qusaiarmoush610@gmail.com>
2023-09-09 02:16:21 +0300

    CanUserGetOrderGuard

--- src/modules/order/guards/can-user-get-order-exist.guard.ts
+++ src/modules/order/guards/can-user-get-order-exist.guard.ts
@@ -1,18 +1,34 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { IsAdminGuard } from 'src/modules/auth/guards/is-admin.guard';
-import { IsOrderMyOrderGuard } from './is-order-my-order-exist.guard';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserService } from 'src/modules/user/user.service';
+import { Order } from '../entities/order.entity';
+import { Repository } from 'typeorm';
+
 @Injectable()
 export class CanUserGetOrderGuard implements CanActivate {
   constructor(
-    private readonly isAdminGuard: IsAdminGuard,
-    private readonly isOrderMyOrderGuard: IsOrderMyOrderGuard,
+    private readonly userService: UserService,
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
   ) {}
-
   async canActivate(context: ExecutionContext) {
-    try {
-      return this.isOrderMyOrderGuard.canActivate(context);
-    } catch (error) {
-      return this.isAdminGuard.canActivate(context);
-    }
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest();
+    const orderId = +request.params.id;
+    const userId = +request.user.sub;
+
+    const user = await this.userService.findById(userId);
+    if (user.userName === 'admin') return true;
+
+    const order = await this.orderRepository.findOneBy({ id: orderId, userId });
+
+    if (order) return true;
+
+    throw new ForbiddenException('It is not your order');
   }
 }
